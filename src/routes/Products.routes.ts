abe52e6fa3e3a9f08720d08c40fc90f5df0016ec
Mauricio <mauhmg@gmail.com>
2023-02-21 11:22:05 -0300

    Alteracao na ordem das rotas'

--- src/routes/Products.routes.ts
+++ src/routes/Products.routes.ts
@@ -6,7 +6,7 @@ import { auth } from "../middlewares/JWTAuth";
 const router = Router()
 const products = new Products()
 
-router.get('/', auth, (req, res) => products.list(req, res));
 router.post('/create', auth, (req, res) => products.create(req, res));
+router.get('/', auth, (req, res) => products.list(req, res));
 
 export { router };
